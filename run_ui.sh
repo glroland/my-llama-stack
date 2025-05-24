c245ae43c7580b47758d7be6f046764272e996d7
Lee Roland <glroland@hotmail.com>
2025-05-24 19:31:56 -0400

    moving ui launch to a script so i can debug endpoint address

---
+++ run_ui.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "LLama Stack Endpoint Address: $LLAMA_STACK_ENDPOINT"
+
+echo "Starting Llama Stack UI..."
+streamlit run /usr/local/lib/python3.10/site-packages/llama_stack/distribution/ui/app.py --server.port=8080 --server.address=0.0.0.0 --logger.level=debug
