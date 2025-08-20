7fe81f72f2fa2878912fab1f36fe7fccf8f059e1
Lee Roland <glroland@hotmail.com>
2025-08-20 18:14:05 -0400

    old llama stack ui fix

--- run_ui.sh
+++ run_ui.sh
@@ -3,4 +3,4 @@
 echo "LLama Stack Endpoint Address: $LLAMA_STACK_ENDPOINT"
 
 echo "Starting Llama Stack UI..."
-streamlit run /usr/local/lib/python3.12/site-packages/llama_stack/distribution/ui/app.py --server.port=8080 --server.address=0.0.0.0 --logger.level=debug
+streamlit run /usr/local/lib/python3.12/site-packages/llama_stack/core/ui/app.py --server.port=8080 --server.address=0.0.0.0 --logger.level=debug
