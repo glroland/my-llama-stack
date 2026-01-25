a1ad5cac0369e930fb7d075465e26e11569a95b5
Lee Roland <glroland@hotmail.com>
2026-01-25 11:33:17 -0500

    updated to latest patch release, removed ui, added WIP operator config

--- run_ui.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "LLama Stack Endpoint Address: $LLAMA_STACK_ENDPOINT"
-
-echo "Starting Llama Stack UI..."
-streamlit run /usr/local/lib/python3.12/site-packages/llama_stack/core/ui/app.py --server.port=8080 --server.address=0.0.0.0 --logger.level=debug
